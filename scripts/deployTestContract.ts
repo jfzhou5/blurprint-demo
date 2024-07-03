7230edbdd04c3888ba0e2d5f37d6712820415cee
jfzhou5 <1241330802@qq.com>
2024-07-03 16:09:28 +0800

    test extends [mutates] fun

--- scripts/deployTestContract.ts
+++ scripts/deployTestContract.ts
@@ -3,7 +3,7 @@ import { TestContract } from '../wrappers/TestContract';
 import { NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const testContract = provider.open(await TestContract.fromInit());
+    const testContract = provider.open(await TestContract.fromInit(1n));
 
     await testContract.send(
         provider.sender(),
@@ -13,10 +13,14 @@ export async function run(provider: NetworkProvider) {
         {
             $$type: 'Deploy',
             queryId: 0n,
-        }
+        },
     );
 
     await provider.waitForDeploy(testContract.address);
 
     // run methods on `testContract`
+
+    console.log('getIsZero', await testContract.getIsZero(1n));
+    console.log('getAddOne', await testContract.getAddOne(1n));
+    console.log('customPow', await testContract.getCustomPow(2n, 2n));
 }
